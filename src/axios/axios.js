745fc68ca20aec1c6244d22d3f0c045ffaf9e7ba
Romanov Oleg <ya.pochta96@gmail.com>
2022-06-05 14:00:01 +0300

    fix

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -8,7 +8,10 @@ const Axios = axios.create({
 });
 
 axios.interceptors.request.use(function (config) {
-  config.headers.Authorization = localStorage.getItem('AccessToken');
+  const token = localStorage.getItem('AccessToken');
+  if (token) {
+    config.headers.Authorization = 'Bearer' + token;
+  }
   return config;
 }, function (error) {
   // Do something with request error
